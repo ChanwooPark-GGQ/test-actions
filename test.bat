6f003e64db445de6f55d571b6a1a21d0062f377b
박찬우QA <165117617+ChanwooPark-GGQ@users.noreply.github.com>
2024-07-15 16:01:34 +0900

    git commit 수정

--- test.bat
+++ test.bat
@@ -1,6 +1,7 @@
 @echo off
 set TIMESTAMP=%date/_%_%time::=_%
-set RESULTS_FILE=results_%TIMESTAMP%.log
+@REM set RESULTS_FILE=results_%TIMESTAMP%.log
+set RESULTS_FILE=results.log
 
 
 date /t > %RESULTS_FILE%
